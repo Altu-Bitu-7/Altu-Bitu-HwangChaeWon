115ddaf5c4f053631f58e4b973f6f1499db6dd64
zzangss <164142490+zzangss@users.noreply.github.com>
2024-10-01 19:15:13 +0900

    [동적 계획법] 10월 1일 주석추가

--- BOJ_2240.cpp
+++ BOJ_2240.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 
-int plum[1001][3];
+int plum[1001][3]; //자두의 시간별 떨어지는 위치
 int T, W;
 
 int DP()
 {
-    int result = 0;         // 최종 결과
-    int count[1001][32][3]; // 자두 개수 세는 배열
+    int result = 0;         
+    int count[1001][32][3]; 
 
-    count[1][0][1] = plum[1][1];
+    // 자두의 시작 위치를 두 경우로 나누어 저장해놓기
+    count[1][0][1] = plum[1][1]; 
     count[1][1][2] = plum[1][2];
 
     result = max(count[1][0][1], count[1][1][2]);
@@ -20,8 +21,9 @@ int DP()
     {
         for (int j = 0; j <= W; j++)
         {
-            count[i][j][1] = max(count[i - 1][j][1], count[i - 1][j - 1][2]) + plum[i][1];
-            count[i][j][2] = max(count[i - 1][j - 1][1], count[i - 1][j][2]) + plum[i][2];
+            //자두의 위치에 따라 값을 업데이트 해주기
+            count[i][j][1] = max(count[i - 1][j][1], count[i - 1][j - 1][2]) + plum[i][1]; 
+            count[i][j][2] = max(count[i - 1][j - 1][1], count[i - 1][j][2]) + plum[i][2]; 
 
             result = max(max(result, count[i][j][1]), count[i][j][2]);
         }
@@ -37,7 +39,7 @@ int main()
     for (int i = 1; i <= T; i++)
     {
         int num;
-        scanf("%d", &num);
+        cin >> num;
         plum[i][num] = 1;
     }
 
